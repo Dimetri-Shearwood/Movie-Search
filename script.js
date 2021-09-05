401d38b8881721f8456fa5583404b65c26e26aee
dshearwood <d.algorhythmic@gmail.com>
2021-09-05 19:32:46 -0400

    Got the current picture displayed, with title, plot, rating, and review

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
-let selectElements = (selector) => document.querySelectorAll(selector)
-let selectAnElement = (selector) => document.querySelector(selector)
-let createAnElement = (elementType) => document.createElement(elementType)
-let appendAChild = (theParent, theChild) => theParent.appendChild(theChild)
-
+let selectElements = (selector) => document.querySelectorAll(selector);
+let selectAnElement = (selector) => document.querySelector(selector);
+let createAnElement = (elementType) => document.createElement(elementType);
+let appendAChild = (theParent, theChild) => theParent.appendChild(theChild);
 
 let createdDiv = createAnElement("div");
 let createdImg = createAnElement("img");
 let movieContainer = selectAnElement(".container");
+let recentContainer = selectAnElement(".grid-container");
 let currentPicture = selectAnElement(".current");
+let recent1 = selectAnElement(".pic1")
+let recent2 = selectAnElement(".pic2")
+let recent3 = selectAnElement(".pic3")
 
 // movieContainer.appendChild(createdDiv);
 // currentPicture.appendChild(createdImg);
 appendAChild(movieContainer, createdDiv);
+appendAChild(recentContainer, createdDiv);
 appendAChild(currentPicture, createdImg);
+appendAChild(currentPicture, createdImg);
+appendAChild(recent1, createdImg);
+appendAChild(recent2, createdImg);
+appendAChild(recent3, createdImg);
 
-const URL = "www.fetchcalldemo.com";
+
+const URL = "www.fetchcalldemo.com"; //omdb api
 
 let getData = async (url) => {
   let response = await getData(url);
   let data = await response.json();
   console.log(data);
-}
-getData(URL);
-
+};
+// getData(URL);
 
 let allShows = {
   Title: "Guardians of the Galaxy Vol. 2",
@@ -66,8 +74,69 @@ let allShows = {
   Response: "True",
 };
 
-//Create a function to display picture. Then 
-displayCurrentPic = () => {
-  allShows.Poster
-}
+//I am trying to show a poster from a url holding API data.
+//It is located in poster.
+//Located in allShows.Poster
+// I need to use the image element I've already created
+// I have to fill the image src attribute with allShows.Poster
+//?? Figure out how to add a src with a DOM ??
+// I dont know what to do next
+currentFullDisplay = () => {
+  displayCurrentPic = () => {
+    console.log(allShows.Poster);
+    createdImg.src = allShows.Poster;
+    // allShows.Poster.classList.add("currentPicture")
+  };
+  displayCurrentPic();
 
+  displayCurrentTitle = (title) => {
+    console.log(allShows.Title);
+    let heading = document.createElement("h1");
+    heading.innerHTML = allShows.Title;
+    appendAChild(currentPicture, heading);
+  };
+  displayCurrentTitle();
+
+  displayCurrentPlot = (description) => {
+    console.log(allShows.Plot);
+    let plot = document.createElement("p");
+    plot.innerHTML = allShows.Plot;
+    appendAChild(currentPicture, plot);
+  };
+  displayCurrentPlot();
+
+  displayCurrentRelease = (releaseDate) => {
+    console.log(allShows.Released);
+    let released = document.createElement("time");
+    released.innerHTML = `Release Date: ${allShows.Released}`;
+    appendAChild(currentPicture, released);
+  };
+  displayCurrentRelease();
+
+  displayCurrentRating = (rating) => {
+    console.log(allShows.Ratings);
+    let ratings = document.createElement("h5");
+    ratings.innerHTML = `Rating: ${allShows.Ratings[1]}`;
+    appendAChild(currentPicture, ratings);
+  };
+  displayCurrentRating();
+};
+currentFullDisplay();
+
+allRecentDisplays = () => {
+//I am trying to pass all of the information from current picture to the first pic1 last 3 searched
+//All the info is located in currentFullDisplay function
+// I have all the info for current and could possibly run the same type of code
+//I believe it would be a if statement to where if someone searches and the search goes through and there isnt an error, then push the current info to pic1, and the same for pic2 and pic3.
+  //Figure out how to send the same info to pic1 class
+  displayLastPic1 = () => {
+    console.log(allShows.Poster);
+    
+    createdImg.src = allShows.Poster;
+    
+    // allShows.Poster.classList.add("currentPicture")
+  };
+  displayLastPic1();
+
+}
+allRecentDisplays();
