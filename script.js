b0fd13bdd03d9e4c36778cbac0e97bf1551c33da
dshearwood <d.algorhythmic@gmail.com>
2021-09-09 11:06:56 -0400

    Cleaned up code, organized, fetched the correct API

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ let recent1 = selectAnElement(".pic1")
 let recent2 = selectAnElement(".pic2")
 let recent3 = selectAnElement(".pic3")
 
+
 // movieContainer.appendChild(createdDiv);
 // currentPicture.appendChild(createdImg);
 appendAChild(movieContainer, createdDiv);
@@ -22,154 +23,69 @@ appendAChild(currentPicture, createdImg);
 
 
 
-const URL = "www.fetchcalldemo.com"; //omdb api
+let handleSubmit = (e) => {
+  e.preventDefault();
+  console.log(e)
+  // console.log(e.target[1])
+  // console.log(e.target.children)
+
+
+  // What to do next:
+  // 1. Clean up your unneeded code
+  // 2. Update the api call to also accept the film type selection
+  // 3. Update your DOM logic so that it clears out the old data from the Current Search Box
+  // 4. Stretch Goal: Try and redesign so that you are able to keep track of the previously searched film
+  // ie: We still need to render the three previously searched films
+  // Hint: One approach might be to store all api data in a separate array and then render each item/index in the array
+
+  console.log(e.target.children[0].value)
+  console.log(e.target.children[1].value)
+
+  let userProvidedFilmTitle = e.target.children[0].value
+
+  const URL = `http://www.omdbapi.com/?t=${userProvidedFilmTitle}&apikey=aed557e4`;
+
+  console.log(URL)
+  getData(URL);
+
+}
+
+let grabForm = document.querySelector('.searchfield')
+grabForm.addEventListener('submit', handleSubmit)
+
+console.log(grabForm)
+
+let filmData = [
+  // {1st set of data},  
+  // {2nd set of data},
+  // {3rd set of data},
+  // {4th set of data},
+  // {5th set of data},
+]
 
 let getData = async (url) => {
-  let response = await getData(url);
+  let response = await fetch(url);
   let data = await response.json();
-  console.log(data);
-};
-// getData(URL);
 
-let allShows = {
-  Title: "Guardians of the Galaxy Vol. 2",
-  Year: "2017",
-  Rated: "PG-13",
-  Released: "05 May 2017",
-  Runtime: "136 min",
-  Genre: "Action, Adventure, Comedy",
-  Director: "James Gunn",
-  Writer: "James Gunn, Dan Abnett, Andy Lanning",
-  Actors: "Chris Pratt, Zoe Saldana, Dave Bautista",
-  Plot: "The Guardians struggle to keep together as a team while dealing with their personal family issues, notably Star-Lord's encounter with his father the ambitious celestial being Ego.",
-  Language: "English",
-  Country: "United States",
-  Awards: "Nominated for 1 Oscar. 15 wins & 58 nominations total",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg",
-  Ratings: [
-    {
-      Source: "Internet Movie Database",
-      Value: "7.6/10",
-    },
-    {
-      Source: "Rotten Tomatoes",
-      Value: "85%",
-    },
-    {
-      Source: "Metacritic",
-      Value: "67/100",
-    },
-  ],
-  Metascore: "67",
-  imdbRating: "7.6",
-  imdbVotes: "593,526",
-  imdbID: "tt3896198",
-  Type: "movie",
-  DVD: "10 Jul 2017",
-  BoxOffice: "$389,813,101",
-  Production: "Marvel Studios, Walt Disney Pictures",
-  Website: "N/A",
-  Response: "True",
+  displayCurrentFilm(data)
 };
 
-//I am trying to show a poster from a url holding API data.
-//It is located in poster.
-//Located in allShows.Poster
-// I need to use the image element I've already created
-// I have to fill the image src attribute with allShows.Poster
-//?? Figure out how to add a src with a DOM ??
-// I dont know what to do next
-
-displayCurrentFilm = (title, description, releaseDate, rating ) => {
+displayCurrentFilm = (apiData) => {
+  // console.log(apiData)
   let heading = document.createElement("h1");
   let plot = document.createElement("p");
   let ratings = document.createElement("h5");
   let released = document.createElement("time");
 
-  createdImg.src = allShows.Poster;
-  heading.innerHTML = allShows.Title;
-  plot.innerHTML = allShows.Plot;
-  released.innerHTML = `Release Date: ${allShows.Released}`;
-  ratings.innerHTML = `Rating: ${allShows.Ratings[1]}`;
-  
+  createdImg.src = apiData.Poster;
+  heading.innerHTML = apiData.Title;
+  plot.innerHTML = apiData.Plot;
+  released.innerHTML = `Release Date: ${apiData.Released}`;
+  ratings.innerHTML = `Rating: ${apiData.Ratings[0].Value}`;
+
+
   appendAChild(currentPicture, heading);
   appendAChild(currentPicture, plot);
   appendAChild(currentPicture, released);
   appendAChild(currentPicture, ratings);
 }
-
-displayCurrentPic = () => {
-  console.log(allShows.Poster);
-  createdImg.src = allShows.Poster;
-  // allShows.Poster.classList.add("currentPicture")
-};
-displayCurrentTitle = (title) => {
-  console.log(allShows.Title);
-  let heading = document.createElement("h1");
-  heading.innerHTML = allShows.Title;
-  appendAChild(currentPicture, heading);
-};
-displayCurrentPlot = (description) => {
-  console.log(allShows.Plot);
-  let plot = document.createElement("p");
-  plot.innerHTML = allShows.Plot;
-  appendAChild(currentPicture, plot);
-};
-
-
-displayCurrentRelease = (releaseDate) => {
-  console.log(allShows.Released);
-  let released = document.createElement("time");
-  released.innerHTML = `Release Date: ${allShows.Released}`;
-  appendAChild(currentPicture, released);
-};
-
-
-displayCurrentRating = (rating) => {
-  console.log(allShows.Ratings);
-  let ratings = document.createElement("h5");
-  ratings.innerHTML = `Rating: ${allShows.Ratings[1]}`;
-  appendAChild(currentPicture, ratings);
-};
-
-  displayCurrentPic();
-  displayCurrentTitle();
-  displayCurrentPlot();
-  displayCurrentRelease();
-  displayCurrentRating();
-
-
-allRecentDisplays = () => {
-//I am trying to pass all of the information from current picture to the first pic1 last 3 searched
-//All the info is located in currentFullDisplay function
-// I have all the info for current and could possibly run the same type of code
-//I believe it would be a if statement to where if someone searches and the search goes through and there isnt an error, then push the current info to pic1, and the same for pic2 and pic3.
-  //Figure out how to send the same info to pic1 class
-  displayLastPic1 = () => {
-    console.log(allShows.Poster);
-    appendAChild(recent1, createdImg);
-    recent1.src = allShows.Poster;
-    
-    // allShows.Poster.classList.add("currentPicture")
-  };
-   displayLastPic1();
-  displayLastPic2 = () => {
-    console.log(allShows.Poster);
-    appendAChild(recent2, createdImg);
-    recent2.src = allShows.Poster;
-    
-    // allShows.Poster.classList.add("currentPicture")
-  };
-  displayLastPic2();
-  displayLastPic3 = () => {
-    console.log(allShows.Poster);
-    appendAChild(recent3, createdImg);
-    recent3.src = allShows.Poster;
-    
-    // allShows.Poster.classList.add("currentPicture")
-   };
-  displayLastPic3();
-
-}
-allRecentDisplays();
